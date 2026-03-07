dc0dc891f15124fc7a2f3a43530b0af4b8b267e3
Peter <ztjnwu@gmail.com>
2026-03-07 20:37:16 +0000

    0307

--- cpp_inference_runtime/main.cpp
+++ cpp_inference_runtime/main.cpp
@@ -5,40 +5,32 @@
 #include "ops.h"
 #include "graph.h"
 
-int main() {
+int main() 
+{
 
     Tensor input({1,3});
-
     input.data = {1.0f, 2.0f, 3.0f};
-
     Graph graph;
-
     ReLU relu;
     Softmax softmax;
 
     graph.add_op(&relu);
     graph.add_op(&softmax);
-
     auto start = std::chrono::high_resolution_clock::now();
-
     Tensor output = graph.run(input);
-
     auto end = std::chrono::high_resolution_clock::now();
 
-    double latency =
-        std::chrono::duration<double, std::milli>(end - start).count();
+    double latency = std::chrono::duration<double, std::milli>(end - start).count();
 
     std::cout << "Output: ";
-
-    for (float v : output.data) {
+    for (float v : output.data) 
+    {
         std::cout << v << " ";
     }
 
     std::cout << std::endl;
-
     std::cout << "Latency: " << latency << " ms\n";
-
     std::cout << "Inference done\n";
-
+    
     return 0;
 }
