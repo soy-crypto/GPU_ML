258da1ba69175200facaf38bdf3f89dc89de33b7
peterzhu <ztj_nwu@snnu.edu.cn>
2026-03-07 22:23:51 -0800

    0307

--- cpp_inference_runtime/tensor.h
+++ cpp_inference_runtime/tensor.h
@@ -1,20 +1,16 @@
 #pragma once
 #include <vector>
 
-class Tensor {
+class Tensor 
+{
+
 public:
+
     std::vector<float> data;
-    std::vector<int> shape;
+    int rows;
+    int cols;
 
-    Tensor() {}
+    Tensor(int r,int c);
+    float& operator()(int r,int c);
 
-    Tensor(std::vector<int> s) : shape(s) {
-        int total = 1;
-        for (int v : s) total *= v;
-        data.resize(total);
-    }
-
-    int size() const {
-        return data.size();
-    }
 };
