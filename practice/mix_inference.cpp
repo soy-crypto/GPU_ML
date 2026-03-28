e205681c9461083523e7864abdd851564a58b4c8
peter zhu <ztjnwu@gmail.com>
2026-03-28 18:44:17 +0000

    0328

--- practice/mix_inference.cpp
+++ practice/mix_inference.cpp
@@ -78,7 +78,7 @@ class GPUSoftMax: public Operator
 
             // Call kernel
             int block = 256, grid = input.getRows();
-            softmax_kernel<<<grid, block>>>(d_input, d_output, rows, cols);
+            softmax_kernel<<<grid, block>>>(d_input, d_output, input.getRows(), input.getCols();
             cudaGetLastError();
             cudaDeviceSynchronize();
             cudaMemcpy(output.getData(), d_output, bytes, cudaMemcpyDeviceToHost);
@@ -92,5 +92,6 @@ class GPUSoftMax: public Operator
 
         }
 
+};
 
-}
+
